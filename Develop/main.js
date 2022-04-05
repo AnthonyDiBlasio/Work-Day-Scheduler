12e9b41bfacdd57dc1c2d1e63cf27350e09b0b27
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-04-04 21:44:10 -0400

    adding hmtl,css,javascript files

---
+++ Develop/main.js
@@ -0,0 +1,5 @@
+var jumbo = document.querySelector(".jumbotron");
+var display3 = document.querySelector(".display-3");
+var lead = document.querySelector(".lead");
+var currentDay = document.querySelector("#currentDay");
+var container = document.querySelector(".container");
