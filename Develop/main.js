a09ada63d656f6e62044190e2872676afd46bf70
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-04-04 22:11:51 -0400

    adding html updates"

--- Develop/main.js
+++ Develop/main.js
@@ -3,3 +3,9 @@ var display3 = document.querySelector(".display-3");
 var lead = document.querySelector(".lead");
 var currentDay = document.querySelector("#currentDay");
 var container = document.querySelector(".container");
+
+var currentDay = moment().format("dddd, MMMM Do YYYY ");
+$("#currentDay").text(currentDay);
+var lead = moment().format()
+var currentDay = moment().format()
+var container = moment().format()
