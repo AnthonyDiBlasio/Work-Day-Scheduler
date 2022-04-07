7c9422802ec8b0ce911a4dc131a4b0ab48a5c459
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-04-07 18:29:14 -0400

    adding screenshot

--- Develop/main.js
+++ Develop/main.js
@@ -1,21 +1,16 @@
-var jumbo =$(".jumbotron");
-var display3 = $(".display-3");
-var lead = $(".lead");
-var currentDay = $("#currentDay");
-var container = $(".container");
-var textarea = $(".textarea");
-var past= $(".past");
-var present= $(".present");
-var future= $(".future");
-var timeblock = $(".time-block");
 
+// var currentDay is equal to todays date (day,month,year)
 var currentDay = moment().format("dddd, MMMM Do YYYY ");
+//displays currentDay in the html
 $("#currentDay").text(currentDay);
+//var current hour is equal to the current hour of today. in military time 
 var currentHour = moment().format("HH"); {
     console.log(currentHour);
 }
-
-$(".time-block").each(function() {
+//getlocalStorage is a function that can be called to save input field to local storage
+getlocalStorage();
+//using jquery syntax "color-block".each is a jquery function to match time with the appropriate css class
+$(".color-block").each(function() {
     
     let time = $(this).attr("id");
     if (currentHour == time) {
@@ -23,34 +18,29 @@ $(".time-block").each(function() {
     } else if (currentHour < time) {
         $(this).removeClass("present");
         $(this).addClass("future");
-        $(this).click(function(){
-            $(this).append("<input>");
-        });
     } else if (currentHour > time) {
         $(this).removeClass("future");
         $(this).addClass("past");
         
-  
-  
-    
     }
-  
+  return
 });
+// for loop to listen for clicks in all of the buttons on the html then log the value of its previousElementSibling to the consoles local storage.
 let buttons = document.querySelectorAll(".saveBtn")
 for (let i = 0; i < buttons.length; ++i) {
     buttons[i].addEventListener('click', function(event) {
         var inputVal = buttons[i].previousElementSibling.value;
         console.log('input:', inputVal);
-        var Inum = i + 1;
-        localStorage.setItem('id-' + (i + 1) , inputVal);
+        var storageNum = i + 1;
+        localStorage.setItem('id-' + (storageNum) , inputVal);
     })
 };
-function getDate() {
+// this function was called previously: getLocalStorage is a for loop that goes through the newly created local storage and checks to see if its not null. then displays on html
+function getlocalStorage() {
     for(var i =0; i<10; i++){
-        var itemSave = localStorage.getItem("id-"+ i);
-        if(itemSave!=null){
-            document.querySelector("input[id-" + i + "]").value = itemSave
+        var inputSave = localStorage.getItem("id-"+ i);
+        if(inputSave!=null){
+            document.querySelector("input[id-" + i + "]").value = inputSave
         }
     }
 }
-getDate();
